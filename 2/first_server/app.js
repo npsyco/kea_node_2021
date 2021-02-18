1bfb7d25db06709d521e6f8e42ea5d8ac01facc2
npsyco <npsyco@gmail.com>
2021-02-18 11:30:15 +0100

    nodeAPI_GetAll

--- 2/first_server/app.js
+++ 2/first_server/app.js
@@ -1,11 +1,17 @@
 const express = require("express"); //imports as a const, so to be used later
 const app = express();
 
+var fs = require('fs');
+
+var databaseJSONfile =  fs.readFileSync('first_db.json');
+var parsedJSON = JSON.parse(databaseJSONfile);
+console.log(parsedJSON);
+
 //consider what datatype to store your data in
 // allowed to hardcode the id
 
 app.get("/", (req, res) =>  {
-    res.send({message: "first call to /"});
+    res.send({parsedJSON});
 });
 
 app.get("/anotherpath", (req, res) => {

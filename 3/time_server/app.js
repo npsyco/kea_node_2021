e5047e29931af733c757d23e52748f2d66f0c953
npsyco <npsyco@gmail.com>
2021-02-25 11:46:20 +0100

    TimeServerWithISOtime+Time+Day+Month+Date

--- 3/time_server/app.js
+++ 3/time_server/app.js
@@ -3,10 +3,26 @@ const moment = require("moment");
 
 const app = express();
 
-var CurrentDate = moment();
+let now = moment();
+
+app.get("/ISO", (req, res) => {
+    res.send(now.format());
+});
+
+app.get("/time", (req, res) => {
+    res.send(now.format("HH:mm:ss"));
+});
 
 app.get("/day", (req, res) => {
-    res.send(CurrentDate);
-})
+    res.send(now.format("dddd"));
+});
+
+app.get("/month", (req, res) => {
+    res.send(now.format("MMMM"));
+});
+
+app.get("/date", (req, res) => {
+    res.send(now.format("dddd, MMMM Do YYYY"));
+});
 
 app.listen(8119);
